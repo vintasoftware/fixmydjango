39b3d4492a5a9376657b3974baa4ff02c6728b7c
Victor Carrico <victorcarrico@gmail.com>
2015-04-06 19:05:08 -0300

    First commit

---
+++ fix_my_django/urls.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+
+urlpatterns = patterns('',
+    url(r'^error/', include('errors.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
