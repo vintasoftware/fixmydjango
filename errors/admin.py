aafbc2e910e66e926046d400f879827381056847
Victor Carrico <victorcarrico@gmail.com>
2015-04-09 15:26:26 -0300

    Setting django admin with django_wysiwyg

--- errors/admin.py
+++ errors/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from .models import Error
+from .models import Error, Answer
 
-admin.site.register(Error)
+class AnswerAdmin(admin.StackedInline):
+	model = Answer
+	max_num = 1
+
+
+class ErrorAdmin(admin.ModelAdmin):
+	inlines = [AnswerAdmin]
+	change_form_template = 'errors/admin/change_form.html'
+
+admin.site.register(Answer)
+admin.site.register(Error, ErrorAdmin)
