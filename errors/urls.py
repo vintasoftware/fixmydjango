619b5f041dbfc0ed6807029b85168903ebca9758
Victor Carrico <victorcarrico@gmail.com>
2015-04-09 15:22:17 -0300

    Adding error detail url

--- errors/urls.py
+++ errors/urls.py
@@ -2,8 +2,9 @@
 
 from django.conf.urls import patterns, url
 
-from .views import ErrorListView
+from .views import ErrorListView, ErrorDetailView
 
 urlpatterns = patterns('',
     url(r'^$', ErrorListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', ErrorDetailView.as_view(), name='detail'),
 )
