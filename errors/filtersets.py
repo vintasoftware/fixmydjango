cf8b8bc57d528c3728a721a7fa27d36927310c2e
Victor Carrico <victorcarrico@gmail.com>
2015-04-09 15:29:08 -0300

    Setting ErrorFilterset to errors list view

---
+++ errors/filtersets.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+from django.db.models import Q
+
+import django_filters
+
+from django_filters import MethodFilter
+
+from .models import Error
+
+
+class ErrorFilter(django_filters.FilterSet):
+    exception_type = MethodFilter(action='filter_exception')
+
+    class Meta:
+        model = Error
+        fields = ['exception_type']
+        order_by = ['exception_type']
+
+
+    def filter_exception(self, queryset, value):
+        return queryset.filter(Q(exception_type__icontains=value) | 
+            Q(error_message__icontains=value))
