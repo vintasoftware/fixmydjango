b2981b8d44c6a69d30aa34fe8b196739243ad69f
Victor Carrico <victorcarrico@gmail.com>
2015-04-09 15:21:52 -0300

    Adding error detail view

--- errors/views.py
+++ errors/views.py
@@ -1,10 +1,34 @@
 # coding: utf-8
 
-from django.views.generic import ListView
+from django.views.generic import DetailView
+
+from django_filters.views import FilterView
 
 from .models import Error
+from .filtersets import ErrorFilter
 
 
-class ErrorListView(ListView):
-	template_name = 'errors/list.html'
-	model = Error
+class ErrorListView(FilterView):
+    template_name = 'errors/list.html'
+    model = Error
+    filterset_class = ErrorFilter
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ErrorListView, self).get_context_data(*args, **kwargs)
+        errors_filter = {}
+
+        for error in context['error_list']:
+            exception_type = error.exception_type
+            errors_filter[exception_type] = filter(
+                lambda x: x.exception_type == exception_type,
+                context['error_list']
+            )
+
+        context['errors_filter'] = errors_filter
+        return context
+
+
+
+class ErrorDetailView(DetailView):
+    template_name = 'errors/detail.html'
+    model = Error
