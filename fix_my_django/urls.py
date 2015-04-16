f234381f25f661f145f807b2eab4d8d105238e29
Victor Carrico <victorcarrico@gmail.com>
2015-04-16 14:53:40 -0300

    Setting sitemap url

--- fix_my_django/urls.py
+++ fix_my_django/urls.py
@@ -1,10 +1,15 @@
 # coding: utf-8
 
-from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls import patterns, include, url
+from django.contrib.sitemaps.views import sitemap
+
+from errors.sitemaps import ErroSitemap
 
 
 urlpatterns = patterns('',
     url(r'^', include('errors.urls', namespace='errors')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': {'error': ErroSitemap}},
+    name='django.contrib.sitemaps.views.sitemap')
 )
