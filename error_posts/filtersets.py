00dc1025bf68157495cc5fc4ad4928f265f9314b
Flávio Juvenal <flaviojuvenal@gmail.com>
2015-07-06 04:00:35 -0300

    More filters

--- error_posts/filtersets.py
+++ error_posts/filtersets.py
@@ -17,6 +17,14 @@ class ExceptionSearchFilter(django_filters.FilterSet):
         fields = ['exception_type', 'raised_by', 'raised_by_line',
                   'django_version', 'search']
 
+    def __init__(self, *args, **kwargs):
+        super(ExceptionSearchFilter, self).__init__(*args, **kwargs)
+
+        # allow empty in django_version
+        django_version_choices = self.filters['django_version'].extra['choices']
+        self.filters['django_version'].extra['choices'] = (
+            (('', 'All'),) + django_version_choices)
+
     def filter_exception(self, queryset, value):
         return queryset.filter(
             Q(exception_type__icontains=value) |
