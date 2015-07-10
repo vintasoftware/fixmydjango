79dc60e0b6c46cb8f6ab0c2f7f2bf46984b7e21a
Flávio Juvenal <flaviojuvenal@gmail.com>
2015-07-10 15:09:27 -0300

    ErrorPostSerializer with list_url

--- error_posts/serializers.py
+++ error_posts/serializers.py
@@ -1,6 +1,29 @@
+import urllib
+
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from .models import ErrorPost
+
+
+class ErrorPostSearchSerializer(serializers.ListSerializer):
+
+    def _get_list_url(self, request):
+        return "{url}?{query}".format(
+            url=reverse('error_posts:list', request=request),
+            query=urllib.urlencode(request.GET))
+
+    def to_representation(self, data):
+        error_post_list = super(ErrorPostSearchSerializer, self).to_representation(data)
+        return {
+            'error_post_list': error_post_list,
+            'list_url': self._get_list_url(self.context['request'])
+        }
+
+    @property
+    def data(self):
+        # yes we want ListSerializer super, which is BaseSerializer
+        return super(serializers.ListSerializer, self).data
 
 
 class ErrorPostSerializer(serializers.ModelSerializer):
@@ -12,3 +35,4 @@ class ErrorPostSerializer(serializers.ModelSerializer):
         model = ErrorPost
         fields = ('id', 'url', 'exception_type', 'error_message',
                   'raised_by', 'raised_by_line', 'django_version')
+        list_serializer_class = ErrorPostSearchSerializer
