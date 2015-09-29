42061b77ba9f693c22deb8fc689d5ce78406f035
Victor Carrico <victorcarrico@gmail.com>
2015-09-29 15:45:50 -0300

    Refactoring draft browser tests

--- drafts/tests/test_browser.py
+++ drafts/tests/test_browser.py
@@ -9,11 +9,12 @@ from django.core.urlresolvers import reverse
 @pytest.mark.usefixtures('live_server', 'webdriver')
 def test_create_draft_on_browser(live_server, webdriver):
     webdriver.get(live_server.url + reverse('drafts:create'))
-    fields = webdriver.find_elements_by_class_name('form-control')
+    fields = webdriver.find_elements_by_css_selector("""textarea,
+             input[type=text], input[type=email]""")
     for field in fields:
         field.send_keys('draft@test.com')
-    submit = webdriver.find_element_by_xpath("//button[@type='submit']")
+    submit = webdriver.find_element_by_css_selector("button[type=submit]")
     submit.click()
     webdriver.implicitly_wait(10)
 
-    assert webdriver.find_element_by_xpath("//div[@role='alert']")
+    assert webdriver.find_element_by_css_selector("div[role=alert]")
