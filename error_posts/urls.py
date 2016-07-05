5e95610bc357e25380303acc9c8f44368511f0f3
Filipe Ximenes <filipeximenes@gmail.com>
2016-07-05 10:49:12 -0300

    adding how it works section

--- error_posts/urls.py
+++ error_posts/urls.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from .views import ErrorPostListView, ErrorPostDetailView
 from .endpoints import ExceptionSearchAPIView
@@ -9,6 +10,10 @@ from .endpoints import ExceptionSearchAPIView
 urlpatterns = patterns('',
     url(r'^$', ErrorPostListView.as_view(), name='list'),
     url(r'^exceptions/(?P<slug>[\w-]+)/$', ErrorPostDetailView.as_view(), name='detail'),
+
+    url(r'^how-it-works/$',
+        TemplateView.as_view(template_name='error_posts/how_it_works.html'),
+        name='how_it_works'),
 
     url(r'api/search/$', ExceptionSearchAPIView.as_view(), name='api-search'),
 )
